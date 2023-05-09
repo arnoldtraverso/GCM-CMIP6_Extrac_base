8017ab0bab7203408b0667d7eccb7f635dd53c97
Kevin Arnold Traverso <127550441+arnoldtraverso@users.noreply.github.com>
2023-05-09 16:56:18 -0500

    Update

--- 02_Script_GEE/GCM_CMIP6.js
+++ 02_Script_GEE/GCM_CMIP6.js
@@ -18,7 +18,7 @@
 
 // Ingresar el shapefile
 
-var HidroCuenca = ee.FeatureCollection("projects/ee-arnoldtraverso/assets/UH_amazonas");
+var HidroCuenca = ee.FeatureCollection("projects/ee-arnoldtraverso/assets/Shp_huallaga");
 
 // Agregar a mapa, en base a color
 
@@ -29,25 +29,52 @@ var estilo = {
 };
 
 var UHData = HidroCuenca.size().getInfo()
-
 Map.addLayer(HidroCuenca.style(estilo))
 
 // 2. Agregar los atributos para el GCM CMIP6
 
 // Parametros
 
-var listModel = ['ACCESS-CM2', 'ACCESS-ESM1-5', 
-                 'BCC-CSM2-MR', 'CESM2', 'CESM2-WACCM', 'CMCC-CM2-SR5',
-                 'CMCC-ESM2', 'CNRM-CM6-1', 'CNRM-ESM2-1', 'CanESM5', 'EC-Earth3', 'EC-Earth3-Veg-LR',
-                 'FGOALS-g3', 'GFDL-CM4', 'GFDL-ESM4', 'GISS-E2-1-G', 'HadGEM3-GC31-LL', 'HadGEM3-GC31-MM',
-                 'IITM-ESM', 'INM-CM4-8', 'INM-CM5-0', 'IPSL-CM6A-LR', 'KACE-1-0-G', 'KIOST-ESM',
-                 'MIROC-ES2L', 'MIROC6', 'MPI-ESM1-2-HR', 'MPI-ESM1-2-LR', 'MRI-ESM2-0', 'NESM3',
-                 'NorESM2-LM', 'NorESM2-MM', 'TaiESM1', 'UKESM1-0-LL'
+// Modelos que no tienen datos de temperaturas
+//'CESM2', 'CESM2-WACCM', 'IITM-ESM', 'TaiESM1' 
+
+var listModel = ['ACCESS-CM2' 
+                 //'ACCESS-ESM1-5', 
+                 //'BCC-CSM2-MR', 
+                 //'CESM2', 'CESM2-WACCM',
+                 //'CMCC-CM2-SR5', 
+                 //'CMCC-ESM2', 
+                 //'CNRM-CM6-1', 
+                 //'CNRM-ESM2-1',
+                 //'CanESM5', 
+                 //'EC-Earth3', 
+                 //'EC-Earth3-Veg-LR', 
+                 //'FGOALS-g3', 
+                 //'GFDL-CM4',
+                 //'GFDL-ESM4', 
+                 //'GISS-E2-1-G', //v7
+                 //'HadGEM3-GC31-LL',
+                 //'HadGEM3-GC31-MM', 
+                 //'IITM-ESM',
+                 //'INM-CM4-8',
+                 //'INM-CM5-0', //v9
+                 //'IPSL-CM6A-LR', 
+                 //'KACE-1-0-G', 
+                 //'KIOST-ESM', 
+                 //'MIROC-ES2L', 
+                 //'MIROC6', 
+                 //'MPI-ESM1-2-HR',
+                 //'MPI-ESM1-2-LR', 
+                 //'MRI-ESM2-0', 
+                 //'NESM3', 
+                 //'NorESM2-LM', //v14 
+                 //'NorESM2-MM', 
+                 //'UKESM1-0-LL',
+                 //'TaiESM1',
                  ];
                  
-var listScenario = ['historical'];
-var listVariable = ['pr'];
-
+var listScenario = ['ssp245'];     // 'historical', 'ssp245', 'ssp585'
+var listVariable = ['tasmin'];     //pr, tasmin, tasmax
 var listGCMmodel = [];
 
 // Trasformando
@@ -63,16 +90,38 @@ listModel.forEach(function(model){
 
 print('ver lista de modelos seleccionados', listGCMmodel)
 
-// Llevando los datos a mm/dia
+// Llevando los datos a mm/dia y los datos de temperaturas a ºC
 
 var PcpDATA = function(image){
   return image.multiply(86400)
   .copyProperties(image, ["system:time_start"]
   )};
 
+var TempDATA = function(image){
+  return image.subtract(273.15)
+  .copyProperties(image, ["system:time_start"]
+  )};
+
 // Loop para los modelos
 
 var listEXP = ee.List([]);
+
+function extrac(image){
+  var ValuesPCP = image.select(param.variable)
+                  .reduceRegion({
+                    reducer: ee.Reducer.mean(),
+                    geometry: UH,
+                    scale: 25000,
+                    maxPixels: 1e12
+                  }).get(param.variable)
+    
+  var PCP = ee.Feature(null);
+    
+  return ee.Feature(PCP.set('valor',ee.Number(ValuesPCP))
+                      .set('cuenca', 'UH_' + i)
+                      .set('fecha', ee.String(image.date().format('YYYY-MM-DD'))))
+                      
+  }
 
 for (var listGCM = 0; listGCM < listGCMmodel.length; listGCM++){
     var param = {model: listGCMmodel[listGCM][0],      // Modelo GCM
@@ -85,8 +134,23 @@ for (var listGCM = 0; listGCM < listGCMmodel.length; listGCM++){
     
   // Fechas del modelo historico, Formato Año mes dia (YYYY-MM-DD)
   
-  var DateIni = '1990-01-01';
-  var DateFin = '1990-12-31';
+  // var DateIni = '1981-01-01';
+  // var DateFin = '2014-12-31';
+  
+  var DateIni;
+  var DateFin;
+  
+  if(param.scenario !== 'historical'){
+    
+    DateIni = '2015-01-01';
+    DateFin = '2100-12-31';
+    
+  } else if (param.scenario == 'historical'){
+    
+    DateIni = '1981-01-01';
+    DateFin = '2014-12-31';
+    
+  }
   
   // 3. Ejecucion para extrccion de datos
   
@@ -100,27 +164,22 @@ for (var listGCM = 0; listGCM < listGCMmodel.length; listGCM++){
                      .filterMetadata('scenario', 'equals', param.scenario)
                      .filterBounds(UH);
     
-    var SelectGCM = DatasetGCM.select(param.variable).map(PcpDATA);
+    // var SelectGCM = DatasetGCM.select(param.variable).map(PcpDATA);
     
-    function extrac(image){
-      var ValuesPCP = image.select(param.variable)
-                      .reduceRegion({
-                        reducer: ee.Reducer.mean(),
-                        geometry: UH,
-                        scale: 25000,
-                        maxPixels: 1e12
-                      }).get(param.variable)
-                      
-    var PCP = ee.Feature(null);
+    var SelectGCM;
     
-    return ee.Feature(PCP.set('valor',ee.Number(ValuesPCP))
-                         .set('cuenca', 'UH_' + i)
-                         .set('fecha', ee.String(image.date().format('YYYY-MM-DD'))))
-                      
+    if(param.variable == 'pr'){
+      
+      SelectGCM = DatasetGCM.select(param.variable).map(PcpDATA);
+    
+    } else if (param.variable == 'tasmin' || param.variable == 'tasmax') {
+      
+      SelectGCM = DatasetGCM.select(param.variable).map(TempDATA);
+      
     }
     
-    var PCPGCM = SelectGCM.map(extrac);
-    listEXP = listEXP.add(PCPGCM);
+    var GCM = SelectGCM.map(extrac);
+    listEXP = listEXP.add(GCM);
     
   }
   
@@ -135,19 +194,16 @@ var DataEXP = ee.FeatureCollection(listEXP).flatten();
 
 // Estilo de colores
 var rgbVis = {
-  bands: ['pr'],
+  bands: listVariable,
   max: 20,
   min: 0,
   'palette': ['604791', '1d6b99', '39a8a7', '0f8755', '76b349', 'f0af07',
             'e37d05', 'cf513e', '96356f', '724173', '9c4f97', '696969']
 };
 
-// Seleccionado una fecha para plot
-
-var featureCol = ee.ImageCollection(SelectGCM.filterDate('2012-02-01', '2012-02-02'));
-
 // Agregando al mapa
 
+var featureCol = ee.ImageCollection(SelectGCM);
 Map.addLayer(featureCol, rgbVis);
 
 // Estilo para shapefile de cuencas
@@ -166,7 +222,7 @@ Map.addLayer(HidroCuenca.style(estilo2));
 // Crear el gráfico de la serie de tiempo
 // ******************************************************************************
 
-var chartData = DataEXP.select(['fecha', 'valor']).limit(1825);
+var chartData = DataEXP.select(['fecha', 'valor']).limit(5000);
 
 print(chartData)
 
@@ -191,6 +247,6 @@ print(timeSeriesChart)
 Export.table.toDrive({
   collection: DataEXP,
   description: 'GCM_CMIP6_'+ param.variable + '_' + param.scenario,
-  folder: 'GCM_CMIP6' + '_' + param.variable, 
+  folder: 'GCM_CMIP6_huallaga' + '_' + param.variable, 
   fileFormat: 'CSV'
 })
